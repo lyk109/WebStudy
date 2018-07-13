df77d695a151025d9713871c4dfeef13d3f7bca2
liyunkun <1095909109@qq.com>
2018-07-14 02:46:41 +0800

    add messageBoard

--- node.js/test1.js
+++ node.js/test1.js
@@ -1,6 +1,21 @@
-'use strict';
-var foo = require('./test2.js');
-foo.foo();
-foo.foo();
-console.log(2)
-console.log(3)
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+server.on('request', function(req, res) {
+	var path = req.url;
+	if (path == '/') {
+		fs.readFile('index.html', function(err,data) {
+			if(err){
+				res.end('failed to read file');
+			}else{
+				res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+				res.end(data.toString());
+			}
+		})
+	}
+})
+
+server.listen(9000, function() {
+	console.log('server is ready...');
+})
