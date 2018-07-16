c78cf14bcb05e3743dcbb7c3309b78dba91be69a
liyunkun <1095909109@qq.com>
2018-07-16 20:49:47 +0800

    add nodeServer

---
+++ node.js/express.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var app = http.createServer();
+
+app.get = function(url, callback) {
+	app.on('request', function(req, res) {
+		if (req.url === url) {
+			res.send = function(content) {
+				res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+				return res.end(content);
+			};
+			callback(req, res);
+		}
+	})
+}
+
+
+module.exports = function() {
+	return app;
+}
--- node.js/test1.js
+++ node.js/test1.js
@@ -2,14 +2,8 @@ var express = require('express');
 
 var app = express();
 
-// app.use('/public/', express.static('./public/'));
+require('./route')(app);
 
-app.post('/post', function(req, res) {
-	console.log(req);
-})
-
-app.use(express.static('./'));
-
-app.listen(9000, function() {
+app.listen(9001, function() {
 	console.log('app server is online...')
 })
